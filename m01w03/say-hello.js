bf459be20013a97c99c700533754002940ac565c
Pedro A. González <pgonzalez@post.com>
2024-05-27 19:09:51 -0500

    First commit, add initial testing lecture code

---
+++ m01w03/say-hello.js
@@ -0,0 +1,11 @@
+// DECLARING A FUNCTION
+const sayHello = () => 'hello there!';
+// const sayGoodbye = () => 'goodbye!';
+// const getEmoji = () => '😃';
+
+module.exports = sayHello;
+// module.exports = {
+//   sayGoodbye,
+//   getEmoji: getEmoji,
+//   sayHello: sayHello,
+// };
